6eae1a82d563720f50729a0f396ff9475ea20203
ChaoticDawg <t-the-r@hotmail.com>
2016-12-16 10:31:37 -0800

    Edit to check MONGOLAB_URI env var by passinf to home page

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ import src.models.alerts.views
 import src.models.stores.views
 import src.models.users.views
 from src.common.database import Database
+import os
 
 __author__ = 'jslvtr'
 
@@ -20,7 +21,8 @@ def init_db():
 
 @app.route('/')
 def home():
-    return render_template('home.jinja2')
+    mLab_Uri = os.environ.get('MONGOLAB_URI')
+    return render_template('home.jinja2', mLab_Uri)
 
 app.register_blueprint(src.models.users.views.user_blueprint, url_prefix="/users")
 app.register_blueprint(src.models.stores.views.store_blueprint, url_prefix="/stores")
