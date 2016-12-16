78524321d007bdbfb41ba38dd1b20af298a5dc69
ChaoticDawg <t-the-r@hotmail.com>
2016-12-15 20:51:37 -0800

    Change database to hardcoded heroku mongolab DB

--- src/common/database.py
+++ src/common/database.py
@@ -12,7 +12,7 @@ class Database(object):
     @staticmethod
     def initialize():
         client = pymongo.MongoClient(Database.URI)
-        Database.DATABASE = client.get_default_database()
+        Database.DATABASE = client['heroku_7mn53hv3']
 
     @staticmethod
     def insert(collection, data):
